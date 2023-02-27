bd66745b35364a45c14fa2924faca92179b0e852
Irvan Rifaí <ivan.rivai6921@gmail.com>
2023-02-27 08:54:31 +0700

    add bootstrap-vue

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-  <nav>
+  <!-- <nav>
     <router-link to="/">Home</router-link> |
     <router-link to="/about">About</router-link>
-  </nav>
+  </nav> -->
   <router-view/>
 </template>
 
-<style lang="scss">
+<!-- <style lang="scss">
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -27,4 +27,4 @@ nav {
     }
   }
 }
-</style>
+</style> -->
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,17 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/swakarta-icon.png" width="100">
-    <HelloWorld msg="Super App"/>
+    <div>
+      <!-- Image and text -->
+      <b-navbar variant="faded" type="light">
+        <b-navbar-brand href="#">
+          <img src="../assets/swakarta-icon.png" width="50" class="d-inline-block align-top" alt="Swakarta">
+          Swakarta
+        </b-navbar-brand>
+      </b-navbar>
+    </div>
+    <Navbar/>
   </div>
 </template>
 
 <script lang="ts">
 import { Options, Vue } from 'vue-class-component';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
+import Navbar from '@/components/Navbar.vue'; // @ is an alias to /src
 
 @Options({
   components: {
-    HelloWorld,
+    Navbar,
   },
 })
 export default class HomeView extends Vue {}
